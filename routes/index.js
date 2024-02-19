91285fd842efd0ad5d6f029043304d8b874e8f7d
mlvarama <ml.varama12@gmail.com>
2024-02-19 15:26:27 -0600

    Pruebas en red hechas

--- routes/index.js
+++ routes/index.js
@@ -11,19 +11,33 @@ const transporter = createTransport({
   },
 });
 
-const mailOptions = {
-  from: 'reymacorreosweb@gmail.com',
-  to: 'ml.varama12@gmail.com',
-  subject: `Pruebas de SMTP`,
-  text: `Hola`
-};
-
 router.post('/send-email', function (req, res, next) {
+  if (!req.body){
+    res.send({success: false, error: 'No data in body'})
+    return ;
+  }
+  if (!req.body.name || !req.body.phone, !req.body.email, !req.body.subject, !req.body.message){
+    res.send({success: false, error: 'Missing Data in Body'});
+    return ;
+  }
+  const mailOptions = {
+    from: 'reymacorreosweb@gmail.com',
+    to: 'ml.varama12@gmail.com',
+    subject: `${req.body.subject}`,
+    text: `
+      Nombre: ${req.body.name},\n
+      Teléfono: ${req.body.phone},\n
+      Email: ${req.body.email},\n
+      Asunto: ${req.body.subject},\n
+      Mensaje: ${req.body.message},\n
+    `,
+  };
   transporter.sendMail(mailOptions, function(error, info){
       if (error) {
-          res.send(error);
+          res.send({succes: false, error: error});
+          return ;
       } else {
-          res.send('Email sent: ' + info.response)
+          res.send({succes: true, message: 'Email sent: ' + info.response})
       }
   });
 });
